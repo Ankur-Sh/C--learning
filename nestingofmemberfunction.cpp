3082e8425e0097c5fcecf6b4c90d2e22a665bd13
Ankur-Sh <ankursharma.as0403@gmail.com>
2022-09-13 00:38:36 +0530

    nesting in class functions concept

---
+++ nestingofmemberfunction.cpp
@@ -0,0 +1,78 @@
+//OOPs - Class and objects
+
+//C++ --> initially called --> C with classes by stroustroup
+//class --extension of structure (in C)
+//structure had limitations
+//      -members are public
+//      -No methods
+//  classes -->structures+more 
+//  classes -->can have method and properties 
+//  classes -->can make few mebers as private & few as public
+//  structures in C++ are typedefed
+//  you can declare objects along with te class declaration like this:
+        /* class Employee{
+            //Class declaration
+        }harry,rohan,lovish;*/
+//harry.salary=8 makes no sense if salary is private
+
+
+
+    //Nesting of member functions
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class binary{
+    private:
+    void chk_bin(void);
+    string s;
+    public:
+    void read(void);
+   // void chk_bin(void);
+    void ones_compliment(void);
+    void display(void);
+};
+void binary:: read(void){
+    cout<<"Enter a binary number";
+    cin>>s;
+}
+void binary:: chk_bin(void){
+    for (int i = 0; i < s.length(); i++)
+    {
+        if(s.at(i)!='0' && s.at(i)!='1'){
+            cout<<"Incorrect binary format"<<endl;
+            exit(0);
+        }
+    }
+}
+void binary::ones_compliment(void){
+    chk_bin();
+     for (int i = 0; i < s.length(); i++)
+    {
+       if(s.at(i)=='0'){
+           s.at(i)='1';
+       }
+       else{
+           s.at(i)='0';
+       }
+    }
+}
+void binary:: display(void){
+    cout<<"Displaying your binary number"<<endl;
+     for (int i = 0; i < s.length(); i++)
+    {
+      cout<<s.at(i);
+    }
+    cout<<endl;
+}
+int main(){
+    binary b;
+    b.read();
+   // b.chk_bin();
+    b.display();
+    b.ones_compliment();
+    b.display();
+
+return 0;
+}
